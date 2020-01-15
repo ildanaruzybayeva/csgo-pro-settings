ab926de7137cc53d0af943cc9d3ac018e1b68f86
ildanaruzybayeva <ildana.ruzybayeva@gmail.com>
2020-01-15 19:43:49 +0100

    flex adjustment

--- src/pros.js
+++ src/pros.js
@@ -1,62 +1,57 @@
 export const pros = [
   {
-    name: 'S1mple',
-    team: "Na'Vi",
-    dpi: 400,
-    sensitivity: 3.09,
-    rating: 1.29,
+    name: 'Cooming soon',
+    team: 'N/A',
+    dpi: 0,
+    sensitivity: 0,
+    rating: 0,
     number: 1,
-    img:
-      'https://prosettings.net/wp-content/uploads/2018/07/s1mple-profile-picture.png'
+    img: 'https://static.hltv.org/images/playerprofile/blankplayer.svg'
   },
   {
-    name: 'N1ko',
-    team: 'Faze',
-    dpi: 400,
-    sensitivity: 1.465,
-    rating: 1.18,
+    name: 'Cooming soon',
+    team: 'N/A',
+    dpi: 0,
+    sensitivity: 0,
+    rating: 0,
     number: 2,
-    img: 'https://prosettings.net/wp-content/uploads/2017/03/Niko.jpg'
+    img: 'https://static.hltv.org/images/playerprofile/blankplayer.svg'
   },
   {
-    name: 'Stewie2k',
-    team: 'Liquid',
-    dpi: 400,
-    rating: 1.1,
-    sensitivity: 1.8,
+    name: 'Cooming soon',
+    team: 'N/A',
+    dpi: 0,
+    rating: 0,
+    sensitivity: 0,
     number: 3,
-    img:
-      'https://prosettings.net/wp-content/uploads/2018/07/stewie2k-profile-picture.jpg'
+    img: 'https://static.hltv.org/images/playerprofile/blankplayer.svg'
   },
   {
-    name: 'Stewie2k',
-    team: 'Liquid',
-    dpi: 400,
-    rating: 1.1,
-    sensitivity: 1.8,
+    name: 'Cooming soon',
+    team: 'N/A',
+    dpi: 0,
+    rating: 0,
+    sensitivity: 0,
     number: 4,
-    img:
-      'https://prosettings.net/wp-content/uploads/2018/07/stewie2k-profile-picture.jpg'
+    img: 'https://static.hltv.org/images/playerprofile/blankplayer.svg'
   },
   {
-    name: 'Stewie2k',
-    team: 'Liquid',
-    dpi: 400,
-    rating: 1.1,
-    sensitivity: 1.8,
+    name: 'Cooming soon',
+    team: 'N/A',
+    dpi: 0,
+    rating: 0,
+    sensitivity: 0,
     number: 5,
-    img:
-      'https://prosettings.net/wp-content/uploads/2018/07/stewie2k-profile-picture.jpg'
+    img: 'https://static.hltv.org/images/playerprofile/blankplayer.svg'
   },
   {
-    name: 'Stewie2k',
-    team: 'Liquid',
-    dpi: 400,
-    rating: 1.1,
-    sensitivity: 1.8,
+    name: 'Cooming soon',
+    team: 'N/A',
+    dpi: 0,
+    rating: 0,
+    sensitivity: 0,
     number: 6,
-    img:
-      'https://prosettings.net/wp-content/uploads/2018/07/stewie2k-profile-picture.jpg'
+    img: 'https://static.hltv.org/images/playerprofile/blankplayer.svg'
   },
   {
     name: 'NAF',
