13440b889233b210cff84b5adf30bb8756e39125
ildanaruzybayeva <ildana.ruzybayeva@gmail.com>
2020-01-15 18:30:22 +0100

    flexbox

--- src/Player.jsx
+++ src/Player.jsx
@@ -4,19 +4,17 @@ import './Player.css';
 export class Player extends Component {
   render() {
     return (
-      <div className='scene'>
-        <div className='player'>
-          <h1>
-            {this.props.name} | {this.props.team}
-          </h1>
-          <div className='front'>
-            <img src={this.props.img} alt='simple' />
-          </div>
-          <p>DPI: {this.props.dpi}</p>
-          <p>Sensitivity: {this.props.sensitivity}</p>
-          <p>HLTV Rating: {this.props.rating}</p>
-          <div className='back'></div>
+      <div className='player'>
+        <h1>
+          {this.props.name} | {this.props.team}
+        </h1>
+        <div className='front'>
+          <img src={this.props.img} alt='simple' />
         </div>
+        <p>DPI: {this.props.dpi}</p>
+        <p>Sensitivity: {this.props.sensitivity}</p>
+        <p>HLTV Rating: {this.props.rating}</p>
+        <div className='back'></div>
       </div>
     );
   }
--- src/Players.jsx
+++ src/Players.jsx
@@ -1,44 +1,15 @@
 import React, { Component } from 'react';
 import Player from './Player';
 import './Players.css';
+import { pros } from './pros';
 
 export class Players extends Component {
-  static defaultProps = {
-    pros: [
-      {
-        name: 'S1mple',
-        team: "Na'Vi",
-        dpi: 400,
-        sensitivity: 3.09,
-        rating: 1.29,
-        img:
-          'https://prosettings.net/wp-content/uploads/2018/07/s1mple-profile-picture.png'
-      },
-      {
-        name: 'N1ko',
-        team: 'Faze',
-        dpi: 400,
-        sensitivity: 1.465,
-        rating: 1.18,
-        img: 'https://prosettings.net/wp-content/uploads/2017/03/Niko.jpg'
-      },
-      {
-        name: 'Stewie2k',
-        team: 'Liquid',
-        dpi: 400,
-        rating: 1.1,
-        sensitivity: 1.8,
-        img:
-          'https://prosettings.net/wp-content/uploads/2018/07/stewie2k-profile-picture.jpg'
-      }
-    ]
-  };
   render() {
     return (
       <div>
         <h1>CS:GO PRO SETTINGS</h1>
         <div className='prosList'>
-          {this.props.pros.map(p => {
+          {pros.map(p => {
             return (
               <Player
                 name={p.name}
